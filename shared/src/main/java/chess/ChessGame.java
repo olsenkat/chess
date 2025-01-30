6778ec45717fb8dbe0ddb0a63c6703320d456ccc
Katie <kaw285@byu.edu>
2025-01-29 19:48:08 -0700

    return false for now on unimplemented functions

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -192,7 +192,7 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return false; // Need to implement
     }
 
     /**
@@ -203,7 +203,7 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return false; // Need to implement
     }
 
     /**
