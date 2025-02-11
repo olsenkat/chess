551533fede13fde2e07ac53e3e438edf1dd21037
Katie <kaw285@byu.edu>
2025-02-11 10:55:35 -0700

    created base class for GameService

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,21 @@
+package service;
+
+import request_result.*;
+
+public class GameService
+{
+    CreateResult create(CreateRequest r)
+    {
+        return null;
+    }
+
+    JoinResult join(JoinRequest r)
+    {
+        return null;
+    }
+
+    ListResult list(ListRequest r)
+    {
+        return null;
+    }
+}
