4888d32f91507612b960d0d53f70a20b28da0dbe
Katie <kaw285@byu.edu>
2025-02-10 18:59:45 -0700

    Finished set up for Phase 3, set up the test server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,13 @@
 import chess.*;
+import spark.Spark;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        Server newServer = new Server();
+        int port = 8080;
+        newServer.run(port);
     }
 }
