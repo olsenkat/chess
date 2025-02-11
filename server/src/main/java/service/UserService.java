c723c4f98f4ece28472c98b3c4fb022954ba81ab
Katie <kaw285@byu.edu>
2025-02-11 11:30:23 -0700

    Implemented login and register functions

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,17 +1,54 @@
 package service;
 
+import dataaccess.*;
 import request_result.*;
+import model.UserData;
+import model.AuthData;
 
 public class UserService
 {
+    private final UserDAO users = new MemoryUserDAO();
+    private final AuthDAO auth = new MemoryAuthDAO();
     RegisterResult register(RegisterRequest r)
     {
-        return null;
+        UserData new_user = new UserData(r.username(), r.password(), r.email());
+        String token = AuthDAO.generateToken();
+        try {
+            users.createUser(new_user);
+        }
+        catch (DataAccessException e)
+        {
+            return new RegisterResult(null, null, "Error: User Creation Error");
+        }
+        try {
+            auth.createAuth(new AuthData(r.username(),token));
+        }
+        catch (DataAccessException e)
+        {
+            return new RegisterResult(null, null, "Error: Auth Creation Error");
+        }
+        return new RegisterResult(r.username(), token, null);
     }
 
     LoginResult login(LoginRequest r)
     {
-        return null;
+        String token = AuthDAO.generateToken();
+        try
+        {
+            users.getUser(r.username());
+        }
+        catch (DataAccessException e)
+        {
+            return new LoginResult(null, null, "Error: User Login Error");
+        }
+        try {
+            auth.createAuth(new AuthData(r.username(),token));
+        }
+        catch (DataAccessException e)
+        {
+            return new LoginResult(null, null, "Error: Auth Creation Error");
+        }
+        return new LoginResult(r.username(), token, null);
     }
 
     LogoutResult logout(LogoutRequest r)
