e5cb88d7e5329703e6f0c28f7db28201bdc3b868
Katie <kaw285@byu.edu>
2025-02-11 10:52:42 -0700

    Created base functions

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,21 @@
+package service;
+
+import request_result.*;
+
+public class UserService
+{
+    RegisterResult register(RegisterRequest r)
+    {
+        return null;
+    }
+
+    LoginResult login(LoginRequest r)
+    {
+        return null;
+    }
+
+    LogoutResult logout(LogoutRequest r)
+    {
+        return null;
+    }
+}
