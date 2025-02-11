4974b7215dbc5979b9cb637d82dfcf0defb43485
Katie <kaw285@byu.edu>
2025-02-11 14:41:59 -0700

    Updated the create method

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,12 +1,36 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.*;
+import model.GameData;
 import request_result.*;
 
 public class GameService
 {
+    private final UserDAO users = new MemoryUserDAO();
+    private final AuthDAO auth = new MemoryAuthDAO();
+    private final GameDAO games = new MemoryGameDAO();
     CreateResult create(CreateRequest r)
     {
-        return null;
+        int gameID = games.createID();
+        try
+        {
+            auth.getAuth(r.authToken());
+        }
+        catch (DataAccessException e)
+        {
+            return new CreateResult(null, "Error: Unable to access authorization");
+        }
+        try
+        {
+            ChessGame newGame = new ChessGame();
+            games.createGame(new GameData(gameID, null, null, r.gameName(), newGame));
+        }
+        catch (DataAccessException e)
+        {
+            return new CreateResult(null, "Error: Unable to create Game");
+        }
+        return new CreateResult(gameID, null);
     }
 
     JoinResult join(JoinRequest r)
