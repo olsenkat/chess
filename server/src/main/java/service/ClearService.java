13f54abc31017146f58463e1d4db8b9b756d5461
Katie <kaw285@byu.edu>
2025-02-12 11:41:35 -0700

    Implemented clear method

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,11 +1,24 @@
 package service;
 
+import dataaccess.*;
 import request_result.ClearResult;
 
 public class ClearService
 {
+    private final UserDAO users = new MemoryUserDAO();
+    private final AuthDAO auth = new MemoryAuthDAO();
+    private final GameDAO games = new MemoryGameDAO();
     ClearResult clear()
     {
-        return null;
+        // Clear User Data
+        users.clear();
+
+        // Clear Auth Data
+        auth.clear();
+
+        // Clear Game Data
+        games.clear();
+        return new ClearResult();
+
     }
 }
