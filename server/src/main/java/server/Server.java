13b5b1e8efd653077182b98dff87dac3631c24b1
Katie <kaw285@byu.edu>
2025-02-12 12:18:43 -0700

    Initialized all endpoint for the Server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,13 +1,49 @@
 package server;
 
+import com.google.gson.Gson;
+import exception.ResponseException;
+import request_result.CreateRequest;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
 import spark.*;
 
 public class Server {
 
+    private final ClearService clearService;
+    private final UserService userService;
+    private final GameService gameService;
+//    private final WebSocketHandler webSocketHandler;
+
+    public Server(ClearService clearService, UserService userService, GameService gameService) {
+        this.clearService = clearService;
+        this.userService = userService;
+        this.gameService = gameService;
+//        webSocketHandler = new WebSocketHandler();
+    }
+
+    //    private final WebSocketHandler webSocketHandler;
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
+
+//        Spark.webSocket("/ws", webSocketHandler);
+
+        Spark.delete("/db", this::clear);
+        Spark.post("/user", this::registerUser);
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
+
+
+//        Spark.post("/pet", this::addPet);
+//        Spark.get("/pet", this::listPets);
+//        Spark.delete("/pet/:id", this::deletePet);
+//        Spark.delete("/pet", this::deleteAllPets);
+//        Spark.exception(ResponseException.class, this::exceptionHandler);
 
         // Register your endpoints and handle exceptions here.
 
@@ -22,4 +58,60 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
+    private Object clear(Request req, Response res) throws ResponseException {
+//        var pet = new Gson().fromJson(req.body(), Pet.class);
+//        pet = service.addPet(pet);
+//        webSocketHandler.makeNoise(pet.name(), pet.sound());
+//        return new Gson().toJson(pet);
+        return null;
+    }
+
+    private Object registerUser(Request req, Response res) throws ResponseException {
+//        var pet = new Gson().fromJson(req.body(), Pet.class);
+//        pet = service.addPet(pet);
+//        webSocketHandler.makeNoise(pet.name(), pet.sound());
+//        return new Gson().toJson(pet);
+        return null;
+    }
+
+    private Object login(Request req, Response res) throws ResponseException {
+//        var pet = new Gson().fromJson(req.body(), Pet.class);
+//        pet = service.addPet(pet);
+//        webSocketHandler.makeNoise(pet.name(), pet.sound());
+//        return new Gson().toJson(pet);
+        return null;
+    }
+
+    private Object logout(Request req, Response res) throws ResponseException {
+//        var pet = new Gson().fromJson(req.body(), Pet.class);
+//        pet = service.addPet(pet);
+//        webSocketHandler.makeNoise(pet.name(), pet.sound());
+//        return new Gson().toJson(pet);
+        return null;
+    }
+
+    private Object listGames(Request req, Response res) throws ResponseException {
+//        var pet = new Gson().fromJson(req.body(), Pet.class);
+//        pet = service.addPet(pet);
+//        webSocketHandler.makeNoise(pet.name(), pet.sound());
+//        return new Gson().toJson(pet);
+        return null;
+    }
+
+    private Object createGame(Request req, Response res) throws ResponseException {
+        var game = new Gson().fromJson(req.body(), CreateRequest.class);
+        var new_game = gameService.create(game);
+//        webSocketHandler.makeNoise(pet.name(), pet.sound());
+        return new Gson().toJson(new_game);
+    }
+
+    private Object joinGame(Request req, Response res) throws ResponseException {
+//        var pet = new Gson().fromJson(req.body(), Pet.class);
+//        pet = service.addPet(pet);
+//        webSocketHandler.makeNoise(pet.name(), pet.sound());
+//        return new Gson().toJson(pet);
+        return null;
+    }
+
 }
