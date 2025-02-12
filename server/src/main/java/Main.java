e695974bd4e235cf9671e6c01d5a52b75672aeb0
Katie <kaw285@byu.edu>
2025-02-12 12:39:02 -0700

    Added services into initialization

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,4 +1,7 @@
 import chess.*;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
 import spark.Spark;
 import server.Server;
 
@@ -6,7 +9,10 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
-        Server newServer = new Server();
+        ClearService clearService = new ClearService();
+        GameService gameService = new GameService();
+        UserService userService = new UserService();
+        Server newServer = new Server(clearService, userService, gameService);
         int port = 8080;
         newServer.run(port);
     }
