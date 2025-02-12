41228f3db30d92ebd945b9e0ca7f7791cc4ba3d7
Katie <kaw285@byu.edu>
2025-02-12 12:47:16 -0700

    Created Memory Data access classes. Initialized the services.

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import chess.*;
+import dataaccess.*;
 import service.ClearService;
 import service.GameService;
 import service.UserService;
@@ -9,9 +10,14 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
-        ClearService clearService = new ClearService();
-        GameService gameService = new GameService();
-        UserService userService = new UserService();
+        // Initialize data access
+        GameDAO gameDataAccess = new MemoryGameDAO();
+        UserDAO userDataAccess = new MemoryUserDAO();
+        AuthDAO authDataAccess = new MemoryAuthDAO();
+
+        ClearService clearService = new ClearService(userDataAccess, authDataAccess, gameDataAccess);
+        GameService gameService = new GameService(authDataAccess, gameDataAccess);
+        UserService userService = new UserService(userDataAccess, authDataAccess);
         Server newServer = new Server(clearService, userService, gameService);
         int port = 8080;
         newServer.run(port);
