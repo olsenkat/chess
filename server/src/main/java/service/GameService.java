62650e541600d73ac816e209bf0dae804e0f73dd
Katie <kaw285@byu.edu>
2025-02-12 10:01:16 -0700

    Updated join method and created a method to check the team color

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -5,6 +5,8 @@ import dataaccess.*;
 import model.GameData;
 import request_result.*;
 
+import java.util.Objects;
+
 public class GameService
 {
     private final UserDAO users = new MemoryUserDAO();
@@ -35,11 +37,47 @@ public class GameService
 
     JoinResult join(JoinRequest r)
     {
-        return null;
+        GameData currentGame;
+        int gameID = r.gameID();
+        try
+        {
+            auth.getAuth(r.authToken());
+        }
+        catch (DataAccessException e)
+        {
+            return new JoinResult("Error: Unable to access authorization");
+        }
+        try
+        {
+            currentGame = games.getGame(gameID);
+            if (!checkTeamColor(currentGame, r.playerColor()))
+            {
+                return new JoinResult("Error: Team Color is already taken");
+            }
+        }
+        catch (DataAccessException e)
+        {
+            return new JoinResult("Error: Unable to access authorization");
+        }
+
+        return new JoinResult(null);
     }
 
     ListResult list(ListRequest r)
     {
         return null;
+    }
+
+    private boolean checkTeamColor (GameData data, String color)
+    {
+        if (color.equalsIgnoreCase("white"))
+        {
+            return Objects.equals(data.whiteUsername(), "");
+        }
+        else if (color.equalsIgnoreCase("black"))
+        {
+            return Objects.equals(data.blackUsername(), "");
+        }
+        return false;
     }
 }
