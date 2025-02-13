273b7eb84b16887763aef5c9aab62cb23ac88478
Katie <kaw285@byu.edu>
2025-02-12 18:30:45 -0700

    Commented all functionality of MemoryUserDAO

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -6,31 +6,39 @@ import java.util.Hashtable;
 
 public class MemoryUserDAO  implements UserDAO
 {
+    // Database Hashtable to hold userData
     private final Hashtable<String, UserData> userData = new Hashtable<>();
 
+    // Method intended to retrieve user from database
     @Override
     public UserData getUser(String username) throws DataAccessException {
+        // If the database contains the username, return the data retrieved
         if (userData.containsKey(username))
         {
             return userData.get(username);
         }
+        // Else: throw an exception
         throw new DataAccessException("Username is not found in the database.");
     }
 
+    // Add the new user to the database
     @Override
     public UserData createUser(UserData user) throws DataAccessException {
+        // If the database already has this user, raise exception
         if (userData.containsKey(user.username()))
         {
             throw new DataAccessException("Username is taken");
         }
+
+        // Put the username into the database
         userData.put(user.username(), user);
         return userData.get(user.username());
     }
 
+    // Clear the database
     @Override
-    public boolean clear() {
+    public void clear() {
         userData.clear();
-        return true;
     }
 
 }
