3f39ded1d64aed0649eb268d6f71c62ccd673454
Katie <kaw285@byu.edu>
2025-02-15 13:19:51 -0700

    Created a base registerUser function. Created register tests

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,34 +1,114 @@
 package service;
 
-import dataaccess.UserDAO;
-import dataaccess.AuthDAO;
-import dataaccess.MemoryAuthDAO;
-import dataaccess.MemoryUserDAO;
+import dataaccess.*;
 import exception.ResponseException;
-import model.UserData;
-import model.AuthData;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import service.UserService;
+import org.junit.jupiter.api.Nested;
+import request_result.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("User Service Tests")
 class UserServiceTest {
-    static final UserService service = new UserService(new MemoryUserDAO(), new MemoryAuthDAO());
+    static final MemoryAuthDAO authDAO = new MemoryAuthDAO();
+    static final MemoryUserDAO userDAO = new MemoryUserDAO();
+    static final MemoryGameDAO gameDAO = new MemoryGameDAO();
+    static final UserService userService = new UserService(userDAO, authDAO);
+    static final ClearService clear = new ClearService(userDAO, authDAO, gameDAO);
+    String authToken = " ";
 
-//    @BeforeEach
-//    void clear() throws ResponseException
-//    {
-//        service.
-//    }
-
-    @Test
-    void clear() throws ResponseException
+    @BeforeEach
+    void initTests()
     {
+        this.clear();
+    }
+    @Nested
+    @DisplayName("Register Tests")
+    class RegisterTests {
 
+        @Test
+        void registerValidResponse() {
+            registerUser();
+        }
 
+        @Test
+        void registerNullUsername()
+        {
+            assertThrows(ResponseException.class,
+                    () -> userService.register(new RegisterRequest(null,
+                                                          "password",
+                                                             "email")));
+        }
+
+        @Test
+        void registerNullPassword()
+        {
+            assertThrows(ResponseException.class,
+                    () -> userService.register(new RegisterRequest("username",
+                            null,
+                            "email")));
+        }
+
+        @Test
+        void registerNullEmail()
+        {
+            assertThrows(ResponseException.class,
+                    () -> userService.register(new RegisterRequest("username",
+                            "password",
+                            null)));
+        }
+
+        @Test
+        void registerDuplicateUser()
+        {
+            registerUser();
+            assertThrows(ResponseException.class,
+                    () -> userService.register(new RegisterRequest("username",
+                            "password",
+                            "email@email.com")));
+        }
+    }
+
+    private void clear()
+    {
+        clear.clear();
+    }
+
+    private void registerUser()
+    {
+        String username = "username";
+        String password = "password";
+        String email = "email@email.com";
+
+        RegisterResult registerResult = null;
+
+        try {
+            registerResult = userService.register(new RegisterRequest(username, password, email));
+        } catch (ResponseException e) {
+            fail("No exception expected");
+        }
+
+        try
+        {
+            var user = userDAO.getUser("username");
+            if (!Objects.equals(user.password(), password))
+            {
+                fail("registerUser Error: Passwords don't match");
+            }
+            else if (!Objects.equals(user.email(), email))
+            {
+                fail("registerUser Error: Passwords don't match");
+            }
+        }
+        catch (DataAccessException e)
+        {
+            fail("registerUser should have inserted the user into the database");
+        }
+
+        assertNotNull(registerResult, "Creation Result should not be null");
     }
 }
