ffbed5dc511be6a4fe20e9be4411b08ce40744df
Katie <kaw285@byu.edu>
2025-02-19 10:31:50 -0700

    Added helper methods to simplify code

--- shared/src/main/java/chess/piecemoves/BishopMovesCalculator.java
+++ shared/src/main/java/chess/piecemoves/BishopMovesCalculator.java
@@ -8,10 +8,11 @@ import java.util.*;
 
 public class BishopMovesCalculator
 {
+    ArrayList<ChessMove> moves;
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition)
     {
         // Initialize variables used
-        var moves = new ArrayList<ChessMove>();
+        moves = new ArrayList<ChessMove>();
         final int CHESS_WIDTH = 8; // Width of oard
         int chessDown = myPosition.getRow()- 1; // Chess tiles below this piece
         int chessUp = CHESS_WIDTH - myPosition.getRow(); // Chess tiles above this piece
@@ -27,21 +28,9 @@ public class BishopMovesCalculator
         {
             // Get the end position and move to add;
             ChessPosition endPos = new ChessPosition(myPosition.getRow()+i,myPosition.getColumn()+i);
-            ChessMove temp = new ChessMove(myPosition, endPos, null);
-            // Check if there is another piece in that location
-            if (board.getPiece(endPos) != null)
+            if(!addMove(board, myPosition, endPos, teamColor))
             {
-                // If it is yours: ignore. Else: capture and add to moves
-                if (board.getPiece(endPos).getTeamColor()!=teamColor)
-                {
-                    moves.add(temp);
-                }
                 break;
-                // Add functionality to take another piece
-            }
-            else
-            {
-                moves.add(temp);
             }
         }
 
@@ -51,21 +40,9 @@ public class BishopMovesCalculator
         {
             // Get end position and move to add;
             ChessPosition endPos = new ChessPosition(myPosition.getRow()-i,myPosition.getColumn()+i);
-            ChessMove temp = new ChessMove(myPosition, endPos, null);
-            // Check if there is another piece in that location
-            if (board.getPiece(endPos) != null)
+            if(!addMove(board, myPosition, endPos, teamColor))
             {
-                // If it is yours: ignore. Else: capture and add to moves
-                if (board.getPiece(endPos).getTeamColor()!=teamColor)
-                {
-                    moves.add(temp);
-                }
                 break;
-                // Add functionality to take another piece
-            }
-            else
-            {
-                moves.add(temp);
             }
         }
 
@@ -75,21 +52,9 @@ public class BishopMovesCalculator
         {
             // Get end position and move to add;
             ChessPosition endPos = new ChessPosition(myPosition.getRow()-i,myPosition.getColumn()-i);
-            ChessMove temp = new ChessMove(myPosition, endPos, null);
-            // Check if there is another piece in that location
-            if (board.getPiece(endPos) != null)
+            if(!addMove(board, myPosition, endPos, teamColor))
             {
-                // If it is yours: ignore. Else: capture and add to moves
-                if (board.getPiece(endPos).getTeamColor()!=teamColor)
-                {
-                    moves.add(temp);
-                }
                 break;
-                // Add functionality to take another piece
-            }
-            else
-            {
-                moves.add(temp);
             }
         }
 
@@ -99,24 +64,33 @@ public class BishopMovesCalculator
         {
             // Get end position and move to add;
             ChessPosition endPos = new ChessPosition(myPosition.getRow()+i,myPosition.getColumn()-i);
-            ChessMove temp = new ChessMove(myPosition, endPos, null);
-            // Check if there is another piece in that location
-            if (board.getPiece(endPos) != null)
+            if(!addMove(board, myPosition, endPos, teamColor))
             {
-                // If it is yours: ignore. Else: capture and add to moves
-                if (board.getPiece(endPos).getTeamColor()!=teamColor)
-                {
-                    moves.add(temp);
-                }
                 break;
-                // Add functionality to take another piece
-            }
-            else
-            {
-                moves.add(temp);
             }
         }
 
         return moves;
+    }
+
+    private boolean addMove(ChessBoard board, ChessPosition myPosition, ChessPosition endPos, ChessGame.TeamColor teamColor)
+    {
+        ChessMove temp = new ChessMove(myPosition, endPos, null);
+        // Check if there is another piece in that location
+        if (board.getPiece(endPos) != null)
+        {
+            // If it is yours: ignore. Else: capture and add to moves
+            if (board.getPiece(endPos).getTeamColor()!=teamColor)
+            {
+                moves.add(temp);
+            }
+            return false;
+            // Add functionality to take another piece
+        }
+        else
+        {
+            moves.add(temp);
+        }
+        return true;
     }
 }
