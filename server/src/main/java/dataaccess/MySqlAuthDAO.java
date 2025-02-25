060a22786dc5e86c5fc0a577d6b397cd6908ae82
Katie <kaw285@byu.edu>
2025-02-25 09:44:32 -0700

    Implemented createAuth and getAuth

--- server/src/main/java/dataaccess/MySqlAuthDAO.java
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -1,16 +1,55 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
+import exception.ResponseException;
 import model.AuthData;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class MySqlAuthDAO implements AuthDAO{
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT authToken, username FROM auth WHERE authToken=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readAuth(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
     @Override
     public AuthData createAuth(AuthData auth) throws DataAccessException {
-        return null;
+        int id;
+        var statement = """
+                        INSERT INTO auth
+                        (authToken, username)
+                        VALUES (?, ?)
+                        """;
+        try
+        {
+            executeUpdate(statement, auth.authToken(),
+                    auth.username());
+        }
+        catch (ResponseException e)
+        {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+
+        return new AuthData(auth.authToken(),
+                auth.username());
     }
 
     @Override
@@ -21,5 +60,38 @@ public class MySqlAuthDAO implements AuthDAO{
     @Override
     public void clear() {
 
+    }
+
+    private AuthData readAuth(ResultSet rs) throws SQLException {
+        var json = rs.getString("json");
+        return new Gson().fromJson(json, AuthData.class);
+    }
+
+    private String executeUpdate(String statement, Object... params) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    switch (param) {
+                        case String p -> ps.setString(i + 1, p);
+                        case Integer p -> ps.setInt(i + 1, p);
+                        case ChessGame p -> ps.setString(i + 1, p.toString());
+                        case null -> ps.setNull(i + 1, NULL);
+                        default -> {
+                        }
+                    }
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getString(1);
+                }
+
+                return "0";
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
     }
 }
