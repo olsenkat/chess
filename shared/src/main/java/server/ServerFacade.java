3e909daaeef7ef952e8fecc89069401f0c73eb56
Katie <kaw285@byu.edu>
2025-03-03 10:36:17 -0700

    Updated make request. If the object is a GET method, we do not need to write a body, this changes our method to POST. In writeBody, we want to addHeaders; this means we need to add the authorization if it is needed.

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,17 +1,14 @@
 package server;
 
 import com.google.gson.Gson;
-import exception.ErrorResponse;
 import exception.ResponseException;
-import jdk.jfr.Registered;
-import model.GameData;
-import model.UserData;
 import requestresult.*;
 
 import java.io.*;
-import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.*;
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class ServerFacade {
 
@@ -63,14 +60,22 @@ public class ServerFacade {
         return this.makeRequest("DELETE", path, null, ClearResult.class);
     }
 
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass)
+            throws ResponseException {
         try {
             URL url = (new URI(serverUrl + path)).toURL();
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod(method);
-            http.setDoOutput(true);
+            if (Objects.equals(method, "GET"))
+            {
+                addHeaders(request, http);
+            }
+            else
+            {
+                http.setDoOutput(true);
+                writeBody(request, http);
+            }
 
-            writeBody(request, http);
             http.connect();
             throwIfNotSuccessful(http);
             return readBody(http, responseClass);
@@ -83,17 +88,7 @@ public class ServerFacade {
 
     private static void writeBody(Object request, HttpURLConnection http) throws IOException {
         if (request != null) {
-            Field header;
-            try
-            {
-                header = request.getClass().getDeclaredField("authToken");
-                http.addRequestProperty("authorization", header.toString());
-                http.addRequestProperty("Content-Type", "application/json");
-            }
-            catch (NoSuchFieldException e)
-            {
-                http.addRequestProperty("Content-Type", "application/json");
-            }
+            addHeaders(request, http);
             String reqData = new Gson().toJson(request);
             try (OutputStream reqBody = http.getOutputStream()) {
                 reqBody.write(reqData.getBytes());
@@ -130,6 +125,22 @@ public class ServerFacade {
 
     private boolean isSuccessful(int status) {
         return status / 100 == 2;
+    }
+
+    private static void addHeaders(Object request, HttpURLConnection http)
+    {
+        try
+        {
+            Method getAuth = request.getClass().getMethod("authToken");
+            Object authorization = getAuth.invoke(request);
+            http.addRequestProperty("authorization",
+                    (authorization instanceof String) ? (String) authorization : null);
+            http.addRequestProperty("Content-Type", "application/json");
+        }
+        catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e)
+        {
+            http.addRequestProperty("Content-Type", "application/json");
+        }
     }
 
 
