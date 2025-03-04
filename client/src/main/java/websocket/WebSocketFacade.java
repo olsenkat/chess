035ace89f3869496048dbcae3ed9287de45e5266
Katie <kaw285@byu.edu>
2025-03-03 20:41:45 -0700

    Added loggers to WebSocketFacade.

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -10,6 +10,9 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 //need to extend Endpoint for websocket to work properly
 public class WebSocketFacade extends Endpoint {
 
@@ -17,16 +20,20 @@ public class WebSocketFacade extends Endpoint {
     NotificationHandler notificationHandler;
 
 
-    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
+    public WebSocketFacade(String url, NotificationHandler notificationHandler, Logger logger) throws ResponseException {
         try {
+            logger.finer("Beginning WebSocketFacade");
             url = url.replace("http", "ws");
             URI socketURI = new URI(url + "/ws");
             this.notificationHandler = notificationHandler;
 
+            logger.finer("Getting the Web Socket Container");
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            logger.finer("Connecting to server");
             this.session = container.connectToServer(this, socketURI);
 
             //set message handler
+            logger.finer("Adding a message handler");
             this.session.addMessageHandler(new MessageHandler.Whole<String>() {
                 @Override
                 public void onMessage(String message) {
@@ -37,6 +44,7 @@ public class WebSocketFacade extends Endpoint {
         } catch (DeploymentException | IOException | URISyntaxException ex) {
             throw new ResponseException(500, ex.getMessage());
         }
+        logger.finer("Finishing WebSocketFacade");
     }
 
     //Endpoint requires this method, but you don't have to do anything
