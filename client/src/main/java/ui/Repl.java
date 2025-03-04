b5247b9f627816d245b96ec83bb8bc5c7773cdbb
Katie <kaw285@byu.edu>
2025-03-03 19:03:32 -0700

    Created a base repl file

---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,46 @@
+package ui;
+
+import websocket.NotificationHandler;
+import webSocketMessages.Notification;
+
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
+public class Repl implements NotificationHandler {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl, this);
+    }
+
+    public void run() {
+        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.evalPreLogin(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    public void notify(Notification notification) {
+        System.out.println(SET_TEXT_COLOR_RED + notification.message());
+        printPrompt();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + EMPTY + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+
+}
