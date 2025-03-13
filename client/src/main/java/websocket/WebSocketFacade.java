c0f494574196b5f5f6a283c1bc2b2fed5282eb63
Katie <kaw285@byu.edu>
2025-03-13 14:41:12 -0600

    Added base methods to use. Not implemented yet

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -2,6 +2,10 @@ package websocket;
 
 import com.google.gson.Gson;
 import exception.ResponseException;
+import exception.UnauthorizedException;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
 import websocketmessages.Action;
 import websocketmessages.Notification;
 
@@ -49,6 +53,39 @@ public class WebSocketFacade extends Endpoint {
     //Endpoint requires this method, but you don't have to do anything
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    private void sendMessage(RemoteEndpoint remote, ErrorMessage error)
+    {
+
+    }
+
+    private void connect(String username) throws UnauthorizedException
+    {
+        try
+        {
+            UserGameCommand.CommandType command = UserGameCommand.CommandType.CONNECT;
+            String connectString = "username: \"" + username + "\", command: \"" + command + "\"";
+            var action = new Action(Action.Type.ENTER, connectString);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException e) {
+            throw new UnauthorizedException(500, "Error: " + e.getMessage());
+        }
+    }
+
+    private String makeMove(Session session, String username, MakeMoveCommand command) throws UnauthorizedException
+    {
+        return null;
+    }
+
+    private String leaveGame(Session session, String username, UserGameCommand command) throws UnauthorizedException
+    {
+        return null;
+    }
+
+    private String resign(Session session, String username, UserGameCommand command) throws UnauthorizedException
+    {
+        return null;
     }
 
 }
