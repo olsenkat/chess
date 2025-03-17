5c149030ba4ed1f7da9c024ba185af5998426e16
Katie <kaw285@byu.edu>
2025-03-17 11:01:57 -0600

    Added a method to load the game in

--- client/src/main/java/websocket/NotificationHandler.java
+++ client/src/main/java/websocket/NotificationHandler.java
@@ -1,7 +1,9 @@
 package websocket;
 
+import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
 
 public interface NotificationHandler {
     void notify(NotificationMessage notification);
+    void loadGame(LoadGameMessage game);
 }
